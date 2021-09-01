31a9fb50f604c1c04307d5157a1e035b4e877825
grnacs-peter <68342550+grnacs-peter@users.noreply.github.com>
2021-09-01 09:42:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+	
+	
+	$(".main").mouseenter(function() {
+		
+		$(".header1").css("display", "none");
+		$(".header2").css("display", "block");
+		
+	});
+	
+	
+	$(".opening").mouseenter(function() {
+		
+		$(".header2").css("display", "none");
+		$(".header1").css("display", "block");
+		
+	});
+	
+	
+	$(".navi-1").click(function() {
+		
+		$(".opi").css("display", "none");
+		$(".opi-1").css("display", "block");
+		$(".navi").css("background-color", "#fff");
+		$(".navi-1").css("background-color", "#4E7082");
+		
+	});
+	
+	
+	$(".navi-2").click(function() {
+		
+		$(".opi").css("display", "none");
+		$(".opi-2").css("display", "block");
+		$(".navi").css("background-color", "#fff");
+		$(".navi-2").css("background-color", "#4E7082");
+				
+	});
+	
+	
+	$(".navi-3").click(function() {
+		
+		$(".opi").css("display", "none");
+		$(".opi-3").css("display", "block");
+		$(".navi").css("background-color", "#fff");
+		$(".navi-3").css("background-color", "#4E7082");
+				
+	});
+	
+	
+	$(".navi-4").click(function() {
+		
+		$(".opi").css("display", "none");
+		$(".opi-4").css("display", "block");
+		$(".navi").css("background-color", "#fff");
+		$(".navi-4").css("background-color", "#4E7082");
+				
+	});
+	
+	
+	$(".navi-5").click(function() {
+		
+		$(".opi").css("display", "none");
+		$(".opi-5").css("display", "block");
+		$(".navi").css("background-color", "#fff");
+		$(".navi-5").css("background-color", "#4E7082");
+				
+	});
+	
+	
+	$(".navi-6").click(function() {
+		
+		$(".opi").css("display", "none");
+		$(".opi-6").css("display", "block");
+		$(".navi").css("background-color", "#fff");
+		$(".navi-6").css("background-color", "#4E7082");
+				
+	});
+	
+	
+	$(".navi-7").click(function() {
+		
+		$(".opi").css("display", "none");
+		$(".opi-7").css("display", "block");
+		$(".navi").css("background-color", "#fff");
+		$(".navi-7").css("background-color", "#4E7082");
+				
+	});
+	
+});
